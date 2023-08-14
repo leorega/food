47ed5bd945e85ac07891ec00ae94bc1ee5474107
leorega <leonardoregazzoni@gmail.com>
2023-08-14 14:18:12 -0300

    first commit

---
+++ api/src/models/Recipe.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Recipe', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    healthScore: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    stepByStep: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+  },
+  { timestamps: false }
+  );
+};
